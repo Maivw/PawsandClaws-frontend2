23ee9f1f7b8270dd6a43ed8f055957f5008a0391
tran <fidtran@gmail.com>
2020-07-31 17:54:41 -0400

    get all adoption requests show up

--- src/components/AdopterProfile.js
+++ src/components/AdopterProfile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
-import { Button } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import { adopterProfileShowUp } from "../reducers/authentication";
 import { displayAllReqsOfAdopters } from "../reducers/inforManagement";
 
@@ -25,20 +25,59 @@ export default function AdopterProfile(props) {
 
 	return (
 		<div>
-			<div>{adopter.userame}</div>
-			<div>{adopter.email}</div>
-			<div>{adopter.firstName}</div>
-			<div>{adopter.lastName}</div>
-			{/* <div>
-				{" "}
-				Request:
-				{adopter.AdoptionRequests &&
-					adopter.AdoptionRequests.map((req) => (
-						<ul>
-							<li>{req.message}</li>
-						</ul>
+			<Container style={{ backgroundColor: "yellow" }}>
+				<Col>
+					<Row>{adopter.userame}</Row>
+					<Row>{adopter.email}</Row>
+					<Row>{adopter.firstName}</Row>
+					<Row>{adopter.lastName}</Row>
+					<Row></Row>
+				</Col>
+				Your Request:
+				<Row>
+					<Col xl="2" lg="2" md="2" xs="6">
+						Num
+					</Col>
+					<Col xl="2" lg="2" md="2" xs="6">
+						Pet Information
+					</Col>
+					<Col xl="2" lg="2" md="2" xs="6">
+						Shelter Name
+					</Col>
+					<Col xl="2" lg="2" md="2" xs="6">
+						Message
+					</Col>
+					<Col xl="2" lg="2" md="2" xs="6">
+						Send Date
+					</Col>
+					<Col xl="2" lg="2" md="2" xs="6">
+						Is Accepted
+					</Col>
+				</Row>
+				{requests &&
+					requests.map((req) => (
+						<Row>
+							<Col xl="2" lg="2" md="2" xs="6">
+								{req.id}
+							</Col>
+							<Col xl="2" lg="2" md="2" xs="6">
+								<Link to={`/pets/${req.Pet.id}`}>{req.Pet.petName}</Link>
+							</Col>
+							<Col xl="2" lg="2" md="2" xs="6">
+								{req.ShelterUser.shelterName}
+							</Col>
+							<Col xl="2" lg="2" md="2" xs="6">
+								{req.message}
+							</Col>
+							<Col xl="2" lg="2" md="2" xs="6">
+								{req.createdAt}
+							</Col>
+							<Col xl="2" lg="2" md="2" xs="6">
+								{req.isAccepted ? "Yes" : "No"}
+							</Col>
+						</Row>
 					))}
-			</div> */}
+			</Container>
 		</div>
 	);
 }
